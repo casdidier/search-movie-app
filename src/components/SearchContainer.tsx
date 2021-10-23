2b65db912edae7ef010d1888bf182ad214f259e3
casdidier <cheung.didier@gmail.com>
2021-10-23 09:43:01 +0200

    add button to clear the seach

--- src/components/SearchContainer.tsx
+++ src/components/SearchContainer.tsx
@@ -5,6 +5,7 @@ const Input = styled.input`
   height: 30px;
   font-size: 12px;
   width: 100%;
+  min-width: 100px;
   border: 2px solid #aaa;
   border-radius: 4px;
   margin: 8px 0;
@@ -12,24 +13,32 @@ const Input = styled.input`
   padding: 8px;
   box-sizing: border-box;
   transition: 0.3s;
-  padding-left: 50px;
-  cursor: pointer;
+  padding: 2px 23px 2px 30px;
 
   &:focus {
     border-color: dodgerBlue;
     box-shadow: 0 0 8px 0 dodgerBlue;
   }
-
-  :focus + .left-icon {
-    svg {
-      fill: dodgerBlue;
-    }
-  }
 `;
 
-const StyledInput = styled.div``;
+const StyledInput = styled.div`
+  position: relative;
+  display: flex;
+  min-width: 100px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  position: absolute;
+  top: 14px;
+  right: 8px;
+  width: 12px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  background: none;
+  //FIXME: issue with hidden
+  visibility: ${props => (props?.value !== '' ? 'visible' : 'hidden')};
+`;
 
 interface Props {
   setSearchValue: React.Dispatch<React.SetStateAction<string>>;
@@ -42,7 +51,7 @@ export const SearchContainer = ({ value, setSearchValue }: Props): JSX.Element =
   };
 
   return (
-    <StyledInput className="inputWithIcon">
+    <StyledInput>
       <Input
         className="form-control"
         value={value}
