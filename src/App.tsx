b3a01feb5d3c3dc42e0c42b5f433b22fdb233efc
casdidier <cheung.didier@gmail.com>
2021-10-26 14:39:41 +0200

    add conditional rendering of backarrow

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,48 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import { useHistory, BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { fetchPopularMovieList, MovieList, getMovieRequest } from './api';
 import './App.css';
-import { Header } from './components/Header';
 import { Layout } from './components/Layout';
 import MovieDetail from './components/MovieDetail';
 import MovieGallery from './components/MovieGallery';
 import { SearchContainer } from './components/SearchContainer';
 import Wrapper from './components/Wrapper';
-import { GlobalStyles, lightTheme, darkTheme } from './theme';
+import { lightTheme, darkTheme } from './theme';
 
 const App: React.FC = () => {
   const [movieList, setMovieList] = useState<MovieList>([] as MovieList);
-  const [isLoading, setIsLoading] = useState(false);
   const [searchValue, setSearchValue] = useState('');
-
   const [theme, setTheme] = useState('light');
   const isDarkTheme = theme === 'dark';
   const toggleTheme = () => setTheme(isDarkTheme ? 'light' : 'dark');
 
-  const history = useHistory();
-
   useEffect(() => {
     const fetchFromApi = async () => {
-      setIsLoading(true);
-
       let result;
       if (searchValue === '') {
         result = await fetchPopularMovieList();
       } else {
         result = await getMovieRequest(searchValue);
       }
-      setIsLoading(false);
       setMovieList(result.results);
     };
     fetchFromApi();
   }, [searchValue]);
-
-  console.log('theme', theme);
 
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <Router>
         <Switch>
           <Route exact path="/">
-            <Layout toggleTheme={toggleTheme} isDarkTheme={isDarkTheme}>
+            <Layout toggleTheme={toggleTheme} isDarkTheme>
               <Wrapper>
                 <SearchContainer value={searchValue} setSearchValue={setSearchValue} />
                 <MovieGallery movieList={movieList} />
               </Wrapper>
             </Layout>
           </Route>
-          <Layout toggleTheme={toggleTheme} isDarkTheme={isDarkTheme}>
+          <Layout toggleTheme={toggleTheme} isDarkTheme>
             <Route path="/movie/:id" component={MovieDetail} />
           </Layout>
         </Switch>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Switch from '@mui/material/Switch';
 import NightsStayIcon from '@material-ui/icons/NightsStay';
@@ -10,7 +10,7 @@ import { headerLightTheme, headerDarkTheme } from '../theme';
 // FIXME: issue toogling colour with headercontainer
 const HeaderContainer = styled('div')<{
   isDarkTheme?: boolean;
-  children: JSX.Element[] | JSX.Element;
+  children: JSX.Element[];
 }>`
   display: flex;
   flex-flow: row;
@@ -50,6 +50,8 @@ const BackArrow = styled.div`
   box-shadow: 0 0 11px rgba(33,33,33,.2);
 `;
 
+const HOME_URL_PATH = 'http://localhost:3000/';
+
 interface Props {
   appName: string;
   isDarkTheme: boolean;
@@ -58,13 +60,20 @@ interface Props {
 
 export const Header = ({ appName, toggleTheme, isDarkTheme }: Props): JSX.Element => {
   const history = useHistory();
+  const currentURL = window.location.href;
 
-  console.log('theme from header', isDarkTheme);
-  return (
-    <HeaderContainer>
+  let backArrow = <></>;
+  if (currentURL !== HOME_URL_PATH) {
+    backArrow = (
       <BackArrow onClick={() => history.push('/')}>
         <ArrowBackIosOutlinedIcon fontSize="large" />
       </BackArrow>
+    );
+  }
+
+  return (
+    <HeaderContainer>
+      {backArrow}
       <Title>{appName}</Title>
       <ToogleWrapper>
         <LightModeIcon />
